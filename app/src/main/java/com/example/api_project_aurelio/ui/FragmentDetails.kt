87e1624df71044c6cb5d3f70eec2d02a3b99c008
AurelioAlfons <yuroalfons0407@gmail.com>
2024-10-15 14:08:17 +1100

    Gradle Dependencies

--- app/src/main/java/com/example/api_project_aurelio/ui/FragmentDetails.kt
+++ app/src/main/java/com/example/api_project_aurelio/ui/FragmentDetails.kt
@@ -12,11 +12,7 @@ import com.example.api_project_aurelio.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentDetails.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class FragmentDetails : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -38,23 +34,5 @@ class FragmentDetails : Fragment() {
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentDetails.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentDetails().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    companion object
 }
