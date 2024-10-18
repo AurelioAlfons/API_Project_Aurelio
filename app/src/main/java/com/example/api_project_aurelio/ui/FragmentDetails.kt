19ee8c1a4a10a6aeb9725a1b9a2130d73d03840f
AurelioAlfons <yuroalfons0407@gmail.com>
2024-10-18 11:45:32 +1100

    Add go back button in details

--- app/src/main/java/com/example/api_project_aurelio/ui/FragmentDetails.kt
+++ app/src/main/java/com/example/api_project_aurelio/ui/FragmentDetails.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.api_project_aurelio.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,5 +40,11 @@ class FragmentDetails : Fragment() {
         view.findViewById<TextView>(R.id.cardMedium).text = args.medium
         view.findViewById<TextView>(R.id.cardYear).text = args.year.toString()
         view.findViewById<TextView>(R.id.cardDescription).text = args.description
+
+        val backButton = view.findViewById<Button>(R.id.back)
+        backButton.setOnClickListener {
+            // Navigate back to the dashboard fragment
+            findNavController().navigate(R.id.action_fragmentDetails_to_fragmentDashboard)
+        }
     }
 }
